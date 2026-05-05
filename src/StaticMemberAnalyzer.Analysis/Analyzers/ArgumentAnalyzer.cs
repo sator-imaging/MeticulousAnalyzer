37884d93450a70552e979ef0a3e434e60c8724c3
sator-imaging <16752340+sator-imaging@users.noreply.github.com>
2026-05-05 16:19:44 +0900

    Apply suggestions from code review
    Co-authored-by: sator-imaging <16752340+sator-imaging@users.noreply.github.com>

--- src/StaticMemberAnalyzer.Analysis/Analyzers/ArgumentAnalyzer.cs
+++ src/StaticMemberAnalyzer.Analysis/Analyzers/ArgumentAnalyzer.cs
@@ -81,11 +81,14 @@ namespace SatorImaging.StaticMemberAnalyzer.Analysis.Analyzers
 
         private static void AnalyzeArgument(OperationAnalysisContext context)
         {
+            if (context.Operation is not IArgumentOperation op)
+                return;
+
             // Skip if it's part of an attribute, we handle that via SyntaxNodeAction because IArgumentOperation might not be reported for attributes in this Roslyn version.
             if (op.Syntax is AttributeArgumentSyntax)
                 return;
 
-            if (context.Operation is not IArgumentOperation op || op.IsImplicit)
+            if (op.IsImplicit)
                 return;
 
             // Skip if it's an indexer argument.
