d6936375300107e81f711ad7a37149a665966be3
sator-imaging <16752340+sator-imaging@users.noreply.github.com>
2026-05-20 17:39:48 +0900

    simplify (#226)
    * simplify
    
    * f
    
    * r
    
    * r2

--- src/StaticMemberAnalyzer.Analysis/Analyzers/ArgumentAnalyzer.cs
+++ src/StaticMemberAnalyzer.Analysis/Analyzers/ArgumentAnalyzer.cs
@@ -66,7 +66,7 @@ namespace SatorImaging.StaticMemberAnalyzer.Analysis.Analyzers
                 return;
             }
 
-            var argIndex = argListStx.Arguments.IndexOf(argStx);
+            int argIndex = argListStx.Arguments.IndexOf(argStx);
 
             // string or char is allowed if it's the first argument.
             if (!requireReporting &&
@@ -127,55 +127,41 @@ namespace SatorImaging.StaticMemberAnalyzer.Analysis.Analyzers
                 return;
             }
 
+            // Test framework methods are exempt from all checks.
             var invocationOp = argOp.Parent as IInvocationOperation;
-            if (invocationOp != null && Core.IsKnownTestFramework(invocationOp))
+            if (invocationOp != null && IsKnownAssertionMethod(invocationOp))
             {
                 return;
             }
 
             var argValue = argOp.Value;
 
+            // 'null', 'default', or 'default(T)' is not allowed at all.
             if (!IsPossibleOperation(argValue, out var requireReporting))
             {
                 return;
             }
 
-            // int, string or char is allowed if it's the first argument.
-            // But 'null', 'default', or 'default(T)' is not allowed at all.
             if (!requireReporting)
             {
-                if (argStx.Parent is ArgumentListSyntax argListStx &&
-                    argListStx.Arguments.IndexOf(argStx) == 0)
-                {
-                    if (IsOmittableType(argValue, isConstructor: invocationOp == null))
-                    {
-                        return;
-                    }
-                }
+                var methodOrCtorContainer = invocationOp?.TargetMethod.ContainingType
+                                         ?? (argOp.Parent as IObjectCreationOperation)?.Constructor.ContainingType;
 
-                // String, System.Text and System.IO methods and constructors are intentionally allowed.
-                var containingType = invocationOp?.TargetMethod.ContainingType
-                    ?? (argOp.Parent as IObjectCreationOperation)?.Constructor.ContainingType;
-
-                if (containingType is not null)
+                if (methodOrCtorContainer is not null)
                 {
-                    if (containingType.SpecialType == SpecialType.System_String)
+                    if (IsPervasiveSystemLib(methodOrCtorContainer))
                     {
                         return;
                     }
 
-                    if (containingType.ContainingNamespace is INamespaceSymbol
+                    // int, string or char is allowed if it's the first argument.
+                    if (argStx.Parent is ArgumentListSyntax argListStx &&
+                        argListStx.Arguments.IndexOf(argStx) == 0)
+                    {
+                        if (IsOmittableType(argValue, isConstructor: invocationOp == null))
                         {
-                            Name: "Text" or "IO", ContainingNamespace: INamespaceSymbol
-                            {
-                                Name: "System", ContainingNamespace: INamespaceSymbol
-                                {
-                                    IsGlobalNamespace: true
-                                }
-                            }
-                        })
-                    {
-                        return;
+                            return;
+                        }
                     }
                 }
             }
@@ -259,6 +245,27 @@ namespace SatorImaging.StaticMemberAnalyzer.Analysis.Analyzers
             }
 
             return false;
+        }
+
+        private static bool IsKnownAssertionMethod(IInvocationOperation invocation)
+        {
+            return invocation.TargetMethod.ContainingType.Name is "Must" or "Assert" or "Debug";
+        }
+
+        private static bool IsPervasiveSystemLib(INamedTypeSymbol typeSymbol)
+        {
+            // String, System.Text and System.IO methods and constructors are intentionally allowed.
+            return typeSymbol.SpecialType is SpecialType.System_String
+                || typeSymbol.ContainingNamespace is INamespaceSymbol
+                {
+                    Name: "Text" or "IO", ContainingNamespace: INamespaceSymbol
+                    {
+                        Name: "System", ContainingNamespace: INamespaceSymbol
+                        {
+                            IsGlobalNamespace: true,
+                        }
+                    }
+                };
         }
     }
 }
