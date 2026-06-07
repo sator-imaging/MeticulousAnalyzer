2a41a0b27b74063e588df65ce0ae456117598c73
sator-imaging <16752340+sator-imaging@users.noreply.github.com>
2026-06-07 11:36:28 +0900

    Apply suggestion from @sator-imaging

--- src/analysis/Core.cs
+++ src/analysis/Core.cs
@@ -295,12 +295,14 @@ namespace SatorImaging.StaticMemberAnalyzer.Analysis
         );
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        internal static string ToDiagnosticMessageName(this ISymbol symbol) =>
-            symbol switch
+        internal static string ToDiagnosticMessageName(this ISymbol symbol)
+        {
+            return symbol switch
             {
                 INamespaceSymbol ns => ns.IsGlobalNamespace ? "global" : ns.ToDisplayString(),
                 _ => symbol.ToDisplayString(s_diagnosticMessageFormat),
             };
+        }
 
         // string.Create and Concat(ReadOnlySpan) cannot be used in .net standard 2.0
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
