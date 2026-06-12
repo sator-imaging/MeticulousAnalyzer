e5653e0d969a9d0371699c36dde0968c34b754a6
sator-imaging <16752340+sator-imaging@users.noreply.github.com>
2026-06-12 18:26:44 +0900

    Apply suggestion from @sator-imaging

--- src/analysis/Analyzers/ReflectionAnalyzer.cs
+++ src/analysis/Analyzers/ReflectionAnalyzer.cs
@@ -156,13 +156,15 @@ namespace SatorImaging.StaticMemberAnalyzer.Analysis.Analyzers
 
         private static string GetOperationName(IOperation operation)
         {
-            return operation switch
+            var target = operation switch
             {
-                IInvocationOperation invocation => invocation.TargetMethod.ToDiagnosticMessageName(),
-                IMemberReferenceOperation member => member.Member.ToDiagnosticMessageName(),
-                IArgumentOperation argument => argument.Parameter.ToDiagnosticMessageName(),
-                _ => operation.Kind.ToString(),
+                IInvocationOperation invocation => invocation.TargetMethod,
+                IMemberReferenceOperation member => member.Member,
+                IArgumentOperation argument => argument.Parameter,
+                _ => null,
             };
+            
+            return target?.ToDiagnosticMessageName() ?? operation.Kind.ToString();
         }
 
         private static INamedTypeSymbol? GetReflectionReceiverType(IOperation? instance)
