e5dffb420993294374618c28b0172d26f30965f2
sator-imaging <16752340+sator-imaging@users.noreply.github.com>
2026-07-05 15:27:20 +0900

    Apply suggestion from @sator-imaging

--- test/AnalyzerTests/SMA0040_DisposableAnalyzerTests_ObjectInitializer.cs
+++ test/AnalyzerTests/SMA0040_DisposableAnalyzerTests_ObjectInitializer.cs
@@ -8,7 +8,7 @@ using VerifyCS = StaticMemberAnalyzer.Tests.CSharpCodeFixVerifier<
     SatorImaging.StaticMemberAnalyzer.Analysis.Analyzers.DisposableAnalyzer,
     Microsoft.CodeAnalysis.Testing.EmptyCodeFixProvider>;
 
-namespace SatorImaging.StaticMemberAnalyzer.Tests
+namespace SatorImaging.StaticMemberAnalyzer.Tests.AnalyzerTests
 {
     [TestClass]
     public class SMA0040_DisposableAnalyzerTests_ObjectInitializer
