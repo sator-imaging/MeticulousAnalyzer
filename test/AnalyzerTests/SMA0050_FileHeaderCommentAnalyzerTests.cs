843d7bef9559922058f2f3cc33ae725817a228ff
sator-imaging <16752340+sator-imaging@users.noreply.github.com>
2026-07-20 10:20:43 +0900

    Apply suggestions from code review
    Co-authored-by: sator-imaging <16752340+sator-imaging@users.noreply.github.com>

--- test/AnalyzerTests/SMA0050_FileHeaderCommentAnalyzerTests.cs
+++ test/AnalyzerTests/SMA0050_FileHeaderCommentAnalyzerTests.cs
@@ -11,7 +11,7 @@ using System.Linq;
 // The custom test runner is used because the default roslyn verifier runs a
 // `#pragma warning disable` test automatically, but this analyzer is not
 // affected by `#pragma`.
-using VerifyCS = MeticulousAnalyzer.Tests.FileHeaderCommentAnalyzerVerifier;
+using VerifyCS = SatorImaging.MeticulousAnalyzer.Tests.FileHeaderCommentAnalyzerVerifier;
 
 namespace SatorImaging.MeticulousAnalyzer.Tests.AnalyzerTests
 {
